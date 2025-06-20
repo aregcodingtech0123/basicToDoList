412d3ee027c2b75a052ce3e2fe106fc6a16e3b8d
aregcodingtech0123 <ardaege06.aet@gmail.com>
2025-06-20 09:26:37 +0300

    Add files via upload

---
+++ client/src/App.js
@@ -0,0 +1,15 @@
+import React ,{Fragment}from "react";
+
+
+
+import ListTodos from "./components/ListTodos";
+function App() {
+  return (
+    <div style={{margin:'25px'}}>
+      
+      <ListTodos/>
+    </div>
+  );
+}
+
+export default App;
